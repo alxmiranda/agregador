b5310d6798e4ff2018d477429635e3105c640c3c
Alex <amdesignercontato@gmail.com>
2015-10-07 00:39:03 -0300

    Criando task para ouvir alterações em qualquer arquivo css e js

--- dev/grunt/Gruntfile.js
+++ dev/grunt/Gruntfile.js
@@ -88,5 +88,5 @@ module.exports = function(grunt) {
 	  grunt.loadNpmTasks('grunt-contrib-uglify');
 	  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	  grunt.registerTask('default', ['sass', 'uglify']);
+	  grunt.registerTask('default', ['sass', 'uglify', 'watch']);
 }
