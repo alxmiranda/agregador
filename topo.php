bccc0eff9fb54463c31ad65e4bdab522f89d7d84
Alex <amdesignercontato@gmail.com>
2015-10-18 15:57:00 -0200

    Modularizando topo

---
+++ topo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Agregador</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header class="topo">
+	<div class="logo"></div>
+	<div class="nav-e-busca">
+	<div class="container">
+		<div class="container-topo">
+		<div class="icone-menu"></div>
+		<div class="icone-busca"></div>
+		</div>
+	</div>
+	</div>
+</header>
